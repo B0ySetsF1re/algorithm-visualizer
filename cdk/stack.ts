54781cb66a7080d257cb02ad85e9e706af164723
ityshkivskyi <ivan.tyshkivskyi@burberry.com>
2026-03-21 19:43:12 +0100

    refactor(cdk/stack): adjust imports structure

--- cdk/stack.ts
+++ cdk/stack.ts
@@ -1,24 +1,25 @@
-import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
+import { Distribution, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
+import { join } from 'node:path';
 
-export class AlgorithmVisualizerStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class AlgorithmVisualizerStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'StaticSiteBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    const bucket = new Bucket(this, 'StaticSiteBucket', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
-    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+    const distribution = new Distribution(this, 'Distribution', {
       defaultBehavior: {
-        origin: origins.S3BucketOrigin.withOriginAccessControl(bucket),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        origin: S3BucketOrigin.withOriginAccessControl(bucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
       defaultRootObject: 'index.html',
       errorResponses: [
@@ -30,14 +31,14 @@ export class AlgorithmVisualizerStack extends cdk.Stack {
       ],
     });
 
-    new s3deploy.BucketDeployment(this, 'DeployStaticSite', {
-      sources: [s3deploy.Source.asset('../out')],
+    new BucketDeployment(this, 'DeployStaticSite', {
+      sources: [Source.asset(join(__dirname, '../out'))],
       destinationBucket: bucket,
       distribution,
       distributionPaths: ['/*'],
     });
 
-    new cdk.CfnOutput(this, 'DistributionUrl', {
+    new CfnOutput(this, 'DistributionUrl', {
       value: `https://${distribution.distributionDomainName}`,
       description: 'CloudFront distribution URL',
     });
